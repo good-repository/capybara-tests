b4bf054214fc0f94d8f804b4bfea9ffc372e8f54
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2021-12-08 09:50:48 -0300

    corrige paths e realiza primeiro teste

--- test1/features/support/env.rb
+++ test1/features/support/env.rb
@@ -1,1 +1,9 @@
-require 'cucumber'
+require 'capybara/cucumber'
+require 'selenium-webdriver'
+
+Capybara.configure do |config|
+  config.default_driver = :selenium_chrome
+  config.app_host = 'https://terra.com.br'
+  config.default_max_wait_time = 5
+
+end
