e74c1ade035c26171d166fa68f746ecdce332d5e
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2021-12-14 08:12:51 -0300

    checkbox and radiobutton

---
+++ features/specs/radiobox_checkbox.feature
@@ -0,0 +1,7 @@
+#language: pt
+
+@radiobox_checkbox
+Funcionalidade: Marcando radiobox e checkbox
+
+Cenario: Marcar radiobox e checkbox
+Quando eu marco um checkbox um radiobox
---
+++ features/step_definitions/radiobox_checkbox.rb
@@ -0,0 +1,8 @@
+Quando ("eu marco um checkbox um radiobox") do
+  # find('label[for="white"]').click
+  # check('purple', allow_label_click: true)
+  # uncheck('purple', allow_label_click: true)
+  
+  # radiobox:
+  # choose('red', allow_label_click: true)
+end
