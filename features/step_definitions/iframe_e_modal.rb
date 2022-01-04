827fcb503ebec9678263b7b064a136e7a4074a88
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2022-01-04 13:07:59 -0300

    model iframe and modal

---
+++ features/step_definitions/iframe_e_modal.rb
@@ -0,0 +1,15 @@
+Quando ("entro no iframe e preench os campos") do
+  visit '/'
+  
+  # within_frame('iframe_id') do
+  #   #iframe actions
+  # end
+end
+
+Quando ("entro no modal e verifico o texto") do
+  visit '/'
+
+  # within('modal_id') do
+  #   #modal actions
+  # end
+end
